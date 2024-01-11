e3eee1b53c0854dd2a44a105ed656ba46dbc6eee
lucabresolin <lucabresolin@pm.me>
2024-01-11 21:55:51 +0100

    [TRANSFORM DATA 7/7] Se renseigner sur le format Parquet

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Short Data Engineering project, to ensure some skills are mastered before moving on to harder ideas
 
-Python
+Python main language
+
+Apache Parquet :
+- Column-oriented
+- Efficiently compressed
+- fast OLAP request
+- More efficient than CSV, especially to reduce cloud cost
+
+![img.png](img.png)
---
+++ data_transformer/transformer.py
@@ -0,0 +1,41 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.window import Window
+
+
+def main():
+    spark = SparkSession.builder.appName("DataTransformer").getOrCreate()
+    df = spark.read.csv("../extractor_consumer/data/raw/*", header=True)
+    df.show(5)
+
+    ag_df = df.groupby("date").agg(F.sum(F.col("visitors")).alias("visitors"),
+                                   F.first(F.col("unite")).alias("unite"),
+                                   F.first(F.col("id_capteur")).alias("id_capteur"))
+    ag_df.show(5)
+
+    print(f"""nombre de lignes sans compte de visiteurs :  {ag_df.where(F.col("visitors").isNull()).count()}""")
+    ag_df = ag_df.drop("id_capteur")  # drops na col
+    ag_df.count()
+
+    win_df = ((
+        ag_df
+        .withColumn("day_of_week", F.dayofweek("date"))
+        .withColumn("moyenne_roulante",
+                    F.mean("visitors").over(Window.partitionBy("day_of_week").orderBy("date").rowsBetween(-4, -1)))
+        .drop("day_of_week")
+        .orderBy("date")
+    ))
+    win_df.show(5)
+
+    pct_df = ((
+        win_df
+        .withColumn("pct_change",
+                    F.round((100 * (F.col("visitors") - F.col("moyenne_roulante")) / (F.col("moyenne_roulante"))), 2))
+    ))
+    pct_df.show()
+
+    pct_df.write.mode("overwrite").parquet("data/processed")
+
+
+if __name__ == "__main__":
+    main()
